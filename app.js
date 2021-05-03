bb641c120d50dd2518d18f9572a0b62e9950c158
roman <roman.arushanov@gmail.com>
2021-05-03 18:39:13 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+app.use(express.static(path.resolve(__dirname,'static')))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'static', 'main.html'))
+})
+app.use(function (req, res, next) {
+    res.status(404).send("Not Found")
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
+
